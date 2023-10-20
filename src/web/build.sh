6c66f5e28ddd9b1be995c5fb3d713ab9344b3275
Ian Spence <ian@ecnepsnai.com>
2023-10-19 17:30:49 -0700

    Add build script for web

---
+++ src/web/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "${CACHE_KEY}" ]; then
+    CACHE_KEY=badbeef
+fi
+
+rm -rf "dist"
+cd ../certbox/cmd/certgen
+./wasm.sh
+cd ../../../web
+npm i
+NODE_ENV=production CACHE_KEY=${CACHE_KEY} npx webpack --mode production --config webpack.js
