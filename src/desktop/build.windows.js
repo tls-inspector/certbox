feec7a8774942f2c21c55265c6da0e446f3e70ba
Ian Spence <ian@ecnepsnai.com>
2024-08-11 20:51:07 -0700

    Add loading gif to windows

--- src/desktop/build.windows.js
+++ src/desktop/build.windows.js
@@ -5,13 +5,14 @@ async function build(arch) {
     await packager.app('win32', arch);
     await windowsInstaller.createWindowsInstaller({
         appDirectory: 'package\\Certbox-win32-' + arch,
-        outputDirectory: 'package\\artifacts',
-        title: 'Certbox',
-        iconUrl: 'https://raw.githubusercontent.com/tls-inspector/certbox/main/src/frontend/src/icons/certbox.ico',
-        setupIcon: 'src\\icons\\certbox.ico',
         exe: 'certbox.exe',
+        iconUrl: 'https://raw.githubusercontent.com/tls-inspector/certbox/main/src/frontend/src/icons/certbox.ico',
+        loadingGif: 'src\\icons\\loading.gif',
+        noMsi: true,
+        outputDirectory: 'package\\artifacts',
         setupExe: 'certbox_windows_' + arch + '.exe',
-        noMsi: true
+        setupIcon: 'src\\icons\\certbox.ico',
+        title: 'Certbox',
     });
 }
 
